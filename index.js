bfda5dbf939a2d85c6568610965d2a945e90e864
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-06-09 11:16:25 +0300

    refactor: fix bugs, make code more readable

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const crypto = require('crypto');
-const fs     = require('fs');
+const { createHash } = require('node:crypto');
+const { createReadStream } = require('node:fs');
+
+/**
+ * @callback hashCallback
+ * @param {*} err
+ * @param {string=} digest
+ * @return {*}
+ */
+
+/**
+ * @typedef {object} hashOptions
+ * @property {string} file
+ * @property {string=} hash
+ * @property {boolean=} prefix
+ */
 
 /**
  * Creates a SRI hash of the given file
  *
- * @param  {Mixed}    file Accepts either a filename or a dictionary of options
- * @param  {Function} cb   Optional callback
- * @return {Mixed}    Returns a promise if no callback was provided
+ * @param  {(string|hashOptions)} file Accepts either a filename or a dictionary of options
+ * @param  {hashCallback=} cb          Optional callback
+ * @return {Promise<string>|void}      Returns a promise if no callback has been provided
  */
 function hash(file, cb)
 {
@@ -16,35 +30,34 @@ function hash(file, cb)
 	};
 
 	if (typeof file === 'object') {
-		for (let prop in file) {
-			options[prop] = file[prop];
-		}
+		options = { ...options, ...file };
 	}
 	else {
 		options.file = file;
 	}
 
+	if (!options.file) {
+		throw new TypeError('No file specified');
+	}
+
 	const p = new Promise((resolve, reject) => {
-		const input = fs.createReadStream(options.file);
-		const hash  = crypto.createHash(options.hash);
+		const input = createReadStream(options.file);
+		const hash  = createHash(options.hash);
 		hash.setEncoding('base64');
 
 		input.on('end', () => {
 			hash.end();
+			/** @type string */
 			const digest = hash.read();
 			resolve(options.prefix ? `${options.hash}-${digest}` : digest);
 		});
 
 		input.on('error', (e) => reject(e));
-
 		input.pipe(hash);
 	});
 
 	if (typeof cb === 'function') {
-		p
-			.then(digest => cb(null, digest))
-			.catch(cb)
-		;
+		p.then(digest => cb(null, digest)).catch(cb);
 	}
 	else {
 		return p;
@@ -54,4 +67,3 @@ function hash(file, cb)
 module.exports = {
 	hash
 };
-
